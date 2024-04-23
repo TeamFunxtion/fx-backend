afdba04b20e5f3ef049a97a8c452f4810d627a01
Usopp <gwee1216@naver.com>
2024-04-23 21:22:52 +0900

    Create Project

---
+++ src/main/java/com/fx/funxtion/FunxtionApplication.java
@@ -0,0 +1,13 @@
+package com.fx.funxtion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunxtionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunxtionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fx/funxtion/FunxtionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fx.funxtion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunxtionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
