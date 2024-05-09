6368e01a9705e6de790169aac165942e28b65ea3
Usopp <gwee1216@naver.com>
2024-05-09 23:49:27 +0900

    [#9] Feat: 입찰 즉시구매 로직 추가

--- src/main/java/com/fx/funxtion/domain/product/service/BidService.java
+++ src/main/java/com/fx/funxtion/domain/product/service/BidService.java
@@ -6,6 +6,7 @@ import com.fx.funxtion.domain.product.dto.BidCreateRequest;
 import com.fx.funxtion.domain.product.dto.BidCreateResponse;
 import com.fx.funxtion.domain.product.entity.Bid;
 import com.fx.funxtion.domain.product.entity.Product;
+import com.fx.funxtion.domain.product.entity.ProductStatusType;
 import com.fx.funxtion.domain.product.repository.BidRepository;
 import com.fx.funxtion.domain.product.repository.ProductRepository;
 import com.fx.funxtion.global.RsData.RsData;
@@ -31,10 +32,19 @@ public class BidService {
             throw new IllegalArgumentException("포인트 부족으로 입찰할 수 없습니다.");
         }
 
+        boolean isCoolEnded = false; // 즉시구매로 경매종료 여부
+        int bidPrice = bidCreateRequest.getBidPrice().intValue();
+
         // 오픈 경매일때
         if(product.getSalesTypeId().equals("SA01")) {
+
             if(product.getCurrentPrice() >= bidCreateRequest.getBidPrice()) {
                 throw new IllegalArgumentException("입찰가가 현재가보다 낮습니다.");
+            }
+
+            if(product.getCoolPrice() != null && product.getCoolPrice() <= bidPrice) { // 입찰가가 즉구가 보다 클때
+                isCoolEnded = true; // 즉시구매
+                bidPrice = product.getCoolPrice().intValue();
             }
 
             // todo. 기존 낙찰자 포인트 환불해주기
@@ -50,12 +60,17 @@ public class BidService {
             }
         }
 
+        // 포인트 차감
+        member.setPoint(member.getPoint() - bidPrice);
+
         // 현재가 및 낙찰자 갱신
         product.setCurrentPrice(bidCreateRequest.getBidPrice());
         product.setAuctionWinnerId(bidCreateRequest.getBidderId());
 
-        // 포인트 차감
-        member.setPoint(member.getPoint() - bidCreateRequest.getBidPrice().intValue());
+        if(isCoolEnded) { // 즉시구매시 경매종료(거래중으로 상태변경)
+            product.setStatusTypeId(ProductStatusType.ST04.name());
+        }
+
 
         Bid bid = Bid.builder()
                 .product(product)
@@ -69,9 +84,9 @@ public class BidService {
         // todo. 현재가 갱신되었으니 이전 최고 입찰자 환불시키기?
 
         if(bid.getId() != null && bid.getId() > 0) {
-            return RsData.of("200", "입찰 등록 성공!", new BidCreateResponse(bid));
+            return RsData.of("200", !isCoolEnded ? "입찰 등록 성공!" : "바로구매 성공! 판매자와 거래를 진행하시기 바랍니다.", new BidCreateResponse(bid));
         } else {
-            return RsData.of("500", "입찰 등록 실패!");
+            return RsData.of("500", !isCoolEnded ? "입찰 등록 실패!" : "바로구매 실패!");
         }
     }
 }
