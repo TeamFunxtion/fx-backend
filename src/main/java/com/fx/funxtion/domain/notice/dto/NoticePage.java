e83b9ce4111b9163c3f6659eefac02c62358dbb8
RyuYeonWoo <dusdn5218534@gmail.com>
2024-05-10 17:58:57 +0900

    [#16]Feat 공지사항 페이징 구현 중

---
+++ src/main/java/com/fx/funxtion/domain/notice/dto/NoticePage.java
@@ -0,0 +1,12 @@
+package com.fx.funxtion.domain.notice.dto;
+
+
+import lombok.Data;
+
+@Data
+public class NoticePage {
+    private int totalSize;
+    private int currentPage;
+    private int pageSize;
+
+}
