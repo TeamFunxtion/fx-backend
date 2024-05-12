804d79da935aca88fec57ada838a121e185d46a4
smjae96 <tlawodbs0528@naver.com>
2024-05-12 20:59:20 +0900

    [#15] Feat: stomp 설정 / swagger 추가

--- src/main/java/com/fx/funxtion/global/websocket/config/WebSocketConfig.java
+++ src/main/java/com/fx/funxtion/global/websocket/config/WebSocketConfig.java
@@ -3,24 +3,36 @@ package com.fx.funxtion.global.websocket.config;
 import com.fx.funxtion.global.websocket.handler.SocketHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.*;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    @Autowired
-    private SocketHandler socketHandler;
+//    @Autowired
+//    private SocketHandler socketHandler;
+//
+//    /**
+//     * 채팅 메시지 요청을 받아 처리하도록 설정
+//     */
+//    @Override
+//    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+//        registry.addHandler(socketHandler, "/chat")
+//                .setAllowedOriginPatterns("*");
+//    }
 
-    /**
-     * 채팅 메시지 요청을 받아 처리하도록 설정
-     */
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(socketHandler, "/chat")
-                .setAllowedOriginPatterns("*");
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOrigins("*");
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/sub");
+        registry.setApplicationDestinationPrefixes("/pub");
     }
 }
