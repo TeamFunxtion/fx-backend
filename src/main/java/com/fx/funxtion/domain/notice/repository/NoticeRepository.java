34fa3e0bb11a0369eb1d3ef8125b190e6269dcbf
RyuYeonWoo <dusdn5218534@gmail.com>
2024-05-13 18:09:36 +0900

    [#16]Feat 공지사항 페이징 구현 완료

--- src/main/java/com/fx/funxtion/domain/notice/repository/NoticeRepository.java
+++ src/main/java/com/fx/funxtion/domain/notice/repository/NoticeRepository.java
@@ -5,14 +5,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
-
 
 
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
-
-//     List<Notice> findNoticeByOrderByIdDesc();
 
      Page<Notice> findNoticeByOrderByCreateDateDesc(Pageable pageable);
 
