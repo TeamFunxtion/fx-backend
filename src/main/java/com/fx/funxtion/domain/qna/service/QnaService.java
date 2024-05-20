d1fb000d1ccd6439167e15e7d1676d7477c40407
ryu5110 <dusdn5218534@gmail.com>
2024-05-20 18:13:54 +0900

    [#44]Fix: 필수값 체크,답변글 업데이트 기능 추가

--- src/main/java/com/fx/funxtion/domain/qna/service/QnaService.java
+++ src/main/java/com/fx/funxtion/domain/qna/service/QnaService.java
@@ -2,9 +2,7 @@ package com.fx.funxtion.domain.qna.service;
 
 import com.fx.funxtion.domain.member.repository.MemberRepository;
 
-import com.fx.funxtion.domain.qna.dto.QnaCreateRequest;
-import com.fx.funxtion.domain.qna.dto.QnaCreateResponse;
-import com.fx.funxtion.domain.qna.dto.QnaDto;
+import com.fx.funxtion.domain.qna.dto.*;
 import com.fx.funxtion.domain.qna.entity.Qna;
 import com.fx.funxtion.domain.qna.repository.QnaRepository;
 import com.fx.funxtion.global.RsData.RsData;
@@ -49,5 +47,33 @@ public class QnaService {
         Page<QnaDto> list = qnaRepository.findByUserId(userId,pageable).map(QnaDto::new);
 
         return list;
+    }
+
+    @Transactional
+    public Page<QnaDto> getSelectManagerPage(Pageable pageable, int pageNo, int pageSize){
+        pageable = PageRequest.of(pageNo,pageSize, Sort.by(Sort.Direction.DESC,"id"));
+        Page<QnaDto> list = qnaRepository.findBy(pageable).map(QnaDto::new);
+
+        return list;
+    }
+
+    public RsData<QnaUpdateResponse> updateQna(QnaUpdateRequest qnaUpdateRequest){
+        Optional<Qna> optionalQna = qnaRepository.findById(qnaUpdateRequest.getId());
+
+        if(optionalQna.isEmpty()){
+            return RsData.of("500","1:1 문의가 존재하지않습니다");
+        }
+
+        Qna q = optionalQna.get();
+
+        if(qnaUpdateRequest.getQnaAnswer() != null && !qnaUpdateRequest.getQnaAnswer().isEmpty()){
+        q.setQnaAnswer(qnaUpdateRequest.getQnaAnswer());
+        }else{
+            return RsData.of("500","내용이 없습니다");
+        }
+
+        qnaRepository.save(q);
+
+        return RsData.of("200","1:1문의 답변이 등록되었습니다.",new QnaUpdateResponse(q));
     }
 }
